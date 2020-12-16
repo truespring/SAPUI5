fafbb2d82e8035c54da9a38a1c031e0857a9e0ee
truespring <true_spring_@naver.com>
2020-12-16 15:20:03 +0900

    오류 수정

--- Walkthrough/step8 - 복사본/Component.js
+++ Walkthrough/step8 - 복사본/Component.js
@@ -27,7 +27,7 @@ sap.ui.define([
 
             // set i18n model
             var i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n",
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
                 supportedLocales: [""],
                 fallbackLocale: ""
             });
