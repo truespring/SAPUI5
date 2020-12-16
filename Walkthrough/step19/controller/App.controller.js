f33f70e4a595a236f0da8fd1dba2015f12f73f5c
truespring <true_spring_@naver.com>
2020-12-16 17:16:03 +0900

    icon 추가 및 구조 정리

---
+++ Walkthrough/step19/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller"
+], function (Controller, MessageToast) {
+   "use strict";
+   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+
+      onOpenDialog : function() {
+         this.getOwnerComponent().openHelloDialog();
+      }
+   });
+});
