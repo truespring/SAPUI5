637fd47b7a8ddc0a5ab6284e859bdc132d21557a
truespring <true_spring_@naver.com>
2020-12-27 19:43:30 +0900

    오탈자 수정

--- vspace/controller/frame/Left.controller.js
+++ vspace/controller/frame/Left.controller.js
@@ -18,49 +18,49 @@ sap.ui.define([
 		onNavToContent2 : function(oEvent) {
 			console.log("Left.js onNavToContent2 Clicked");
 
-			var oRouter = this.getRoutere();
+			var oRouter = this.getRouter();
 		    oRouter.navTo("contents2");
 		},
 		
 		onNavToContent3 : function(oEvent) {
 			console.log("Left.js onNavToContent3 Clicked");
 
-			var oRouter = this.getRoutere();
+			var oRouter = this.getRouter();
 		    oRouter.navTo("contents3");
 		},
 		
 		onNavToContent4 : function(oEvent) {
 			console.log("Left.js onNavToContent4 Clicked");
 
-			var oRouter = this.getRoutere();
+			var oRouter = this.getRouter();
 		    oRouter.navTo("contents4");
 		},
 		
 		onNavToContent5 : function(oEvent) {
 			console.log("Left.js onNavToContent5 Clicked");
 
-			var oRouter = this.getRoutere();
+			var oRouter = this.getRouter();
 		    oRouter.navTo("contents5");
 		},
 		
 		onNavToContent6 : function(oEvent) {
 			console.log("Left.js onNavToContent6 Clicked");
 
-			var oRouter = this.getRoutere();
+			var oRouter = this.getRouter();
 		    oRouter.navTo("contents6");
 		},
 		
 		onNavToContent7 : function(oEvent) {
 			console.log("Left.js onNavToContent7 Clicked");
 
-			var oRouter = this.getRoutere();
+			var oRouter = this.getRouter();
 		    oRouter.navTo("contents7");
 		},
 		
 		onNavToContent8 : function(oEvent) {
 			console.log("Left.js onNavToContent8 Clicked");
 
-			var oRouter = this.getRoutere();
+			var oRouter = this.getRouter();
 		    oRouter.navTo("contents8");
 		}
 	});
