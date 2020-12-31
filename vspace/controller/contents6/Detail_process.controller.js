367c8c44c51562769a38b31be2f6b88877b85bac
truespring <true_spring_@naver.com>
2020-12-31 15:38:33 +0900

    2020 회사 마지막 커밋

--- vspace/controller/contents6/Detail_process.controller.js
+++ vspace/controller/contents6/Detail_process.controller.js
@@ -16,12 +16,12 @@ sap.ui.define([
         },
 
         // 디테일 화면
-        _onObjectMatched: function (oEvent) {
-            this.getView().bindElement({
-                path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
-                model: "invoice"
-            });
-        },
+        // _onObjectMatched: function (oEvent) {
+        //     this.getView().bindElement({
+        //         path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
+        //         model: "invoice"
+        //     });
+        // },
 
         // 공정설정 & 검색 화면
         onOpenDialog : function () {
@@ -50,6 +50,35 @@ sap.ui.define([
         onSaveDialog : function () {
             alert("저장되었습니다.")
             this.byId("processDialog").close();
+        },
+
+        // 공정관리
+        onOpenManagementDialog : function () {
+            var oView_1 = this.getView();
+
+            if(!this.pDialog) {
+                this.pDialog = Fragment.load({
+                    id: oView_1.getId(),
+                    name: "OpenUI5.view.contents6.processManagement",
+                    controller: this
+                }).then(function (oDialog) {
+                    oView_1.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+
+            this.pDialog.then(function(oDialog) {
+                oDialog.open();
+            });
+        },
+
+        onCloseManagementDialog : function () {
+            this.byId("managementDialog").close();
+        },
+
+        onSaveManagementDialog : function () {
+            alert("저장되었습니다.")
+            this.byId("managementDialog").close();
         }
     });
 });
