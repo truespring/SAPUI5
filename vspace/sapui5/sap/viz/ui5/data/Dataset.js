2383e841796f3dbc12b60645a6aee72d0276a809
truespring <true_spring_@naver.com>
2021-01-03 20:50:45 +0900

    로컬 에러

--- vspace/sapui5/sap/viz/ui5/data/Dataset.js
+++
@@ -1,6 +0,0 @@
-/*!
- * SAPUI5
-
-(c) Copyright 2009-2019 SAP SE. All rights reserved
- */
-sap.ui.define(['sap/ui/core/Element','sap/viz/library'],function(E,l){"use strict";var D=E.extend("sap.viz.ui5.data.Dataset",{metadata:{"abstract":true,library:"sap.viz"}});return D;});
