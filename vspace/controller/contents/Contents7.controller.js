cd4f4f2e81f6c2ca8d5b77208940775de1148d38
truespring <true_spring_@naver.com>
2021-01-04 16:22:06 +0900

    실적관리 페이지 추가 및 README 누락으로 추가

--- vspace/controller/contents/Contents7.controller.js
+++ vspace/controller/contents/Contents7.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
 	"OpenUI5/controller/common/BaseController",
-    "sap/ui/model/json/JSONModel"
-], function(BaseController, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function(BaseController, JSONModel, Filter, FilterOperator) {
 	"use strict";
 		
 	return BaseController.extend("OpenUI5.controller.common.BaseController", {
@@ -13,6 +15,20 @@ sap.ui.define([
 			// set explored app's demo model on this sample
 			var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/contents7.json"));
 			this.getView().setModel(oModel);
-		}
+		},
+        onFilterInvoices : function (oEvent) {
+
+            // build filter array
+            var aFilter = [];
+            var sQuery = oEvent.getParameter("query");
+            if(sQuery) {
+                aFilter.push(new Filter("Name", FilterOperator.Contains, sQuery));
+            }
+
+            // filter binding
+            var oList = this.byId("performance_management");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
+        }
 	});
 }, true);
